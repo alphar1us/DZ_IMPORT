9c2625e9c5278ad2908d0735390230ce6b286d5e
Alphar1is <dudinov.vadim@gmail.com>
2021-07-05 13:55:04 +0300

    create

---
+++ db/people.py
@@ -0,0 +1,4 @@
+def get_employees (name_employees):
+    print(name_employees + ', ты-лучший!')
+
+# get_employees('Альфарий')
---
+++ main.py
@@ -0,0 +1,12 @@
+from application import salary
+from db import people
+# def print_hi(name):
+#     print(f'Hi, {name}')
+
+if __name__ == '__main__':
+    # print_hi('PyCharm')
+    salary.calculate_salary(1,2)
+    people.get_employees('Альфайрий')
+
+
+
