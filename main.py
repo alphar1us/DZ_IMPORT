d024d74e27000b596f75d1f65b77daabba98d6e4
Alphar1is <dudinov.vadim@gmail.com>
2021-07-05 14:12:53 +0300

    add datetime

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import datetime
+
 from application import salary
+from datetime import date
+
 from db import people
 # def print_hi(name):
 #     print(f'Hi, {name}')
 
 if __name__ == '__main__':
     # print_hi('PyCharm')
+    print(date.today())
     salary.calculate_salary(1,2)
     people.get_employees('Альфайрий')
 
